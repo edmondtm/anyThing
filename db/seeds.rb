e13f8a82a86574c880313c618026df846f19ac9c
edmondtm <edmondtm@yahoo.com>
2016-04-28 00:43:11 +0800

    added products to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,10 +9,10 @@
 
 Category.create(category_name:"glassware")
 Subcategory.create(subcategory_name:"beaker")
-Subcategory.create(subcategory_name:"flask")
-Subcategory.create(subcategory_name:"tube")
-Subcategory.create(subcategory_name:"burettes")
-Subcategory.create(subcategory_name:"cylinders")
+Subcategory.create(subcategory_name:"burette")
+Subcategory.create(subcategory_name:"")
+Subcategory.create(subcategory_name:"")
+Subcategory.create(subcategory_name:"")
 
 
 
@@ -68,6 +68,19 @@ product.variations.create(variation_name: "600 ml", variation_sku: "/18SC", vari
 product.variations.create(variation_name: "1000 ml", variation_sku: "/22SC", variation_price: "750.00")
 product.variations.create(variation_name: "2000 ml", variation_sku: "/28SC", variation_price: "960.00")
 
+Product.create(product_name: "Pyrex PTFE key, class AS (works certified) Burette", product_sku: "KSFE-3295", product_brand: "Pyrex", product_description: "<ul><li>Complies with ISO 385</li><li>Individually tested and supplied with cerrtificate showing exact volume dispensed at 3 points on the scale</li><li>Individual serial number and cerrtificate provides complete traceability to National Standards</li><li>Each burette has a +30s waiting time before final reading can be taken</li><li>Manufactured from precision-bore borosilicate glass tubing for high accuracy</li><li>1.5mm bore PTFE key which requires no lubrication</li><li>White enamel graduations</li><li>White enamel graduations</li></ul>", category_id: "1", subcategory_id:"2")
+product = Product.find(5)
+product.variations.create(variation_name: "10 ml - graduations 0.02 ml", variation_sku: "/22ASWC", variation_price: "227.00")
+product.variations.create(variation_name: "25 ml - graduations 0.10 ml", variation_sku: "/24ASWC", variation_price: "203.00")
+product.variations.create(variation_name: "50 ml - graduations 0.10 ml", variation_sku: "/26ASWC", variation_price: "207.00")
+product.variations.create(variation_name: "100 ml - graduations 0.20 ml", variation_sku: "/28ASWC", variation_price: "251.00")
+
+Product.create(product_name: "Pyrex PTFE key, class AS Burette", product_sku: "KSFE-3295", product_brand: "Pyrex", product_description: "<ul><li>Complies with ISO 385</li><li>Manufactured from precision-bore borosilicate glass tubing for high accuracy</li><li>Each burette has a +30s waiting time before final reading can be taken</li><li>Graduations in white enamel for increased visibility with dark solutions</li><li>Grease-free interchangable PTFE key with 1.5mm bore</li></ul>", category_id: "1", subcategory_id:"2")
+product = Product.find(6)
+product.variations.create(variation_name: "10 ml - graduations 0.02 ml", variation_sku: "/12AS", variation_price: "153.00")
+product.variations.create(variation_name: "25 ml - graduations 0.10 ml", variation_sku: "/14AS", variation_price: "129.00")
+product.variations.create(variation_name: "50 ml - graduations 0.10 ml", variation_sku: "/16AS", variation_price: "137.00")
+product.variations.create(variation_name: "100 ml - graduations 0.20 ml", variation_sku: "/18AS", variation_price: "178.00")
 
 
 
